5f995693ab316ad582fd0c33c1b0ede7a66d5c41
Ifeanyi <alichiifeanyi@yahoo.com>
2021-07-15 22:09:18 +0100

    edits

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -11,7 +11,9 @@ class WelcomeScreen extends StatefulWidget {
 
 class _WelcomeScreenState extends State<WelcomeScreen>
     with SingleTickerProviderStateMixin {
+  
   late AnimationController controller;
+  late Animation animation;
 
   @override
   void initState() {
@@ -20,20 +22,37 @@ class _WelcomeScreenState extends State<WelcomeScreen>
     super.initState();
     controller = AnimationController(
       vsync: this,
-      duration: Duration(seconds: 2),
+      duration: Duration(seconds: 1),
     );
+
+    animation = ColorTween(begin: Colors.red, end: Colors.blue).animate(controller);
 
     controller.forward();
 
+    // animation.addStatusListener((status) { 
+    //   if (status == AnimationStatus.completed) {
+    //     controller.reverse(from: 1.0);
+    //   } else if (status == AnimationStatus.dismissed) {
+    //     controller.forward();
+    //   }
+    // });
+
     controller.addListener(() {
       setState(() {});
+      print(animation.value);
     });
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.red.withOpacity(controller.value),
+      backgroundColor: animation.value,
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 24.0),
         child: Column(
@@ -46,11 +65,11 @@ class _WelcomeScreenState extends State<WelcomeScreen>
                   tag: 'logo',
                   child: Container(
                     child: Image.asset('images/logo.png'),
-                    height: 60.0,
+                    height: 60,
                   ),
                 ),
                 Text(
-                  'kings Chat',
+                  'Kings Chat',
                   style: TextStyle(
                     fontSize: 45.0,
                     fontWeight: FontWeight.w900,
