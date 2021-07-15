1aa79cf333ec5334a00a55adedb278b469dfc1c9
Ifeanyi <alichiifeanyi@yahoo.com>
2021-07-15 12:19:19 +0100

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:kings_chat/screens/welcome_screen.dart';
+import 'package:kings_chat/screens/login_screen.dart';
+import 'package:kings_chat/screens/registration_screen.dart';
+import 'package:kings_chat/screens/chat_screen.dart';
+
+void main() => runApp(KingsChat());
+
+class KingsChat extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        textTheme: TextTheme(
+          bodyText2: TextStyle(color: Colors.black54),
+        ),
+      ),
+      initialRoute: WelcomeScreen.id,
+      routes: {
+        WelcomeScreen.id: (context) => WelcomeScreen(),
+        'login_screen': (context) => LoginScreen(),
+        'registration_screen': (context) => RegistrationScreen(),
+        'chat_screen': (context) => ChatScreen(),
+      },
+    );
+  }
+}
