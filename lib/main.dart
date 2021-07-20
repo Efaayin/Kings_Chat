0dd86610004c04157427648355997f027d62d76f
Ifeanyi <alichiifeanyi@yahoo.com>
2021-07-20 14:52:38 +0100

    edits

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,20 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:kings_chat/screens/welcome_screen.dart';
 import 'package:kings_chat/screens/login_screen.dart';
 import 'package:kings_chat/screens/registration_screen.dart';
 import 'package:kings_chat/screens/chat_screen.dart';
 
-void main() => runApp(KingsChat());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(KingsChat());
+}
 
 class KingsChat extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    
     return MaterialApp(
       initialRoute: WelcomeScreen.id,
       routes: {
