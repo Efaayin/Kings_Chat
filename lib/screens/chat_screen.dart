b0454ed600ddf547a5146a5ec5d46269cd25328f
Ifeanyi <alichiifeanyi@yahoo.com>
2021-07-22 16:37:54 +0100

    edits

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -3,8 +3,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:kings_chat/constants.dart';
 
-  final _fireStore = FirebaseFirestore.instance;
-  late User loggedInUser;
+final _fireStore = FirebaseFirestore.instance;
+late User loggedInUser;
 
 class ChatScreen extends StatefulWidget {
   static const String id = 'chat_screen';
@@ -121,9 +121,20 @@ class MessagesStream extends StatelessWidget {
         List<MessageBubble> messageBubbles = [];
         for (var message in messages) {
           final messageDetails = message.data() as Map<String, dynamic>;
+          final messageText = messageDetails['text'];
+          final messageSender = messageDetails['sender'];
+
+          final currentUser = loggedInUser.email;
+
+          if (currentUser == messageDetails['sender']) {
+            // The message from the logged in user
+          }
 
           final messageBubble = MessageBubble(
-              sender: messageDetails['sender'], text: messageDetails['text']);
+            sender: messageSender,
+            text: messageText,
+            isMe: currentUser == messageSender,
+          );
 
           messageBubbles.add(messageBubble);
         }
@@ -142,10 +153,11 @@ class MessagesStream extends StatelessWidget {
 }
 
 class MessageBubble extends StatelessWidget {
-  const MessageBubble({required this.sender, required this.text});
+  MessageBubble({required this.sender, required this.text, required this.isMe});
 
   final String sender;
   final String text;
+  final bool isMe;
 
   @override
   Widget build(BuildContext context) {
@@ -162,9 +174,12 @@ class MessageBubble extends StatelessWidget {
             ),
           ),
           Material(
-            borderRadius: BorderRadius.only(topLeft: Radius.circular(30.0), bottomLeft: Radius.circular(30.0), bottomRight: Radius.circular(30.0)),
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(30.0),
+                bottomLeft: Radius.circular(30.0),
+                bottomRight: Radius.circular(30.0)),
             elevation: 5.0,
-            color: Colors.lightBlueAccent,
+            color: isMe ? Colors.lightBlueAccent : Colors.white,
             child: Padding(
               padding: EdgeInsets.symmetric(
                 horizontal: 20.0,
